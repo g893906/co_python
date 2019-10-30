96289f20207b7d6aa91e2b03982e9fc513e619c8
U-WNEWEB\19001130 <19001130@T1-1-1-Q-37555.wneweb.com.tw>
2019-10-30 16:38:36 +0800

    Updating the time zone cal.

--- time_zone.py
+++ time_zone.py
@@ -2,7 +2,10 @@
 
 string = input("please input the TW time, this program will cal. the IL and ALT time:")
 print("TW time is:"+string)
-ALT_time=int(string)+12
+if(int(string)<12):
+    ALT_time=int(string)+12
+else:
+    ALT_time=int(string)-12
 if (int(string)>=6):
     Israel_time=int(string)-6
 else:
