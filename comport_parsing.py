7aa556b30b50ea3365cb63cb2b01972087f5408e
U-WNEWEB\19001130 <19001130@T1-1-1-Q-37555.wneweb.com.tw>
2019-10-30 16:37:19 +0800

    Updating the com port parsing condition

--- comport_parsing.py
+++ comport_parsing.py
@@ -21,7 +21,7 @@ import sys
 
 ser = serial.Serial()
 
-ser.port = "/dev/ttyS15" #set the com port number
+ser.port = "/dev/ttyS13" #set the com port number
 
 ser.baudrate = 115200     #set the baudrate
 ser.bytesize = serial.EIGHTBITS #number of bits per bytes
@@ -46,7 +46,9 @@ if(chk_path != True):
     os.mkdir(log_folder_path)
 print (log_folder_path)
 
-wlog = open(log_folder_path+"/log.txt","w")
+logtime = time.time()
+st_time = datetime.datetime.fromtimestamp(logtime).strftime('%Y-%m-%d %H:%M:%S')
+wlog = open(log_folder_path+"/"+st_time+".txt","a")
 
 try:
     ser.open()
@@ -66,64 +68,98 @@ if ser.isOpen():
                 numOfLines = 0
 
                 while True:
-                        response = ser.readline()
                         response = ser.readline().decode('utf-8')
                         #msg = 'read data %s.' %response
-                        if ( 'bytes' in response):
-                            logtime = time.time()
-                            st_time = datetime.datetime.fromtimestamp(logtime).strftime('%Y-%m-%d %H:%M:%S')
-                            wlog.write(st_time+' '+response)
-                            if ( (numOfLines%2) >0 ):
-                                ser.setRTS(False)
-                                ser.setDTR(False)
-                            else:
-                                ser.setRTS(True)
-                                ser.setDTR(True) 
+                        #if ( 'bytes' in response):
+                        #    logtime = time.time()
+                        #    st_time = datetime.datetime.fromtimestamp(logtime).strftime('%Y-%m-%d %H:%M:%S')
+                        #    wlog.write(st_time+' '+response)
+                        #    if ( (numOfLines%2) >0 ):
+                        #        ser.setRTS(False)
+                        #        ser.setDTR(False)
+                        #    else:
+                        #        ser.setRTS(True)
+                        #        ser.setDTR(True) 
+                        #    print(response)
+                        #    numOfLines = numOfLines + 1
+                        #    #if (numOfLines >= 5):
+                        #    #    break
+                        if ( 'ZynqMP>' in response):
                             print(response)
-                            numOfLines = numOfLines + 1
-                            #if (numOfLines >= 5):
-                            #    break
-                        elif ( 'ZynqMP>' in response):
-                            print(response)
+                            ser.write("mii write 5 d 1f\n".encode() )
+                            ser.write("mii write 5 e 86\n".encode() )
+                            ser.write("mii write 5 d 401f\n".encode() )
+                            ser.write("mii write 5 e 0x08\n".encode() )
+                            ser.write("mii read 5 e\n".encode() )
+                            wlog.write(response)
+                            #wlog.close()
                             ser.write("bootm\n".encode())
                         elif ( 'login:' in response):
                             print(response)
                             time.sleep(2)
-                            ser.write("root\n".encode())
+                            ser.write("root\r\n".encode())
+                            time.sleep(2)
+                            ser.write("root\r\n".encode())
                         elif ( 'Password:' in response):
                             print(response)
                             time.sleep(2)
-                            ser.write("root\n".encode())
+                            ser.write("root\r\n".encode())
+                        elif ( 'Login incorrect' in response):
+                            print(response)
                         elif ( 'root@se_gsi_apu_18_wnc:~#' in response ):
                             print(response)
                             logtime = time.time()
+                            data='ifconfig eth0 192.168.33.10\n'
+                            data=data.encode()
+                            ser.write(data)
+                            data='ping -c 100 192.168.33.60\n'.encode()
+                            ser.write(data)
+                        elif ( 'packets trans' in response ):
+                            print(response)
+                            logtime = time.time()
                             st_time = datetime.datetime.fromtimestamp(logtime).strftime('%Y-%m-%d %H:%M:%S')
-                            if flag==0:
-                                data='ifconfig eth0 192.168.33.10\n'
-                                #data=data.encode("utf-8").decode("cp950","ignore")
-                                data=data.encode()
-                                ser.write(data)
-                                flag=flag+1;
-                                wlog.write(st_time+' '+"ifconfig")
-                            elif flag==1:
-                                data="tftp -g -r BOOT.bin 192.168.33.60\n"
-                                data=data.encode()
-                                ser.write(data)
-                                wlog.write(st_time+' '+"tftp")
-                                flag=flag+1;
-                            elif flag==2:
-                                data="mv BOOT.bin BOOT1.bin\n"
-                                data=data.encode()
-                                ser.write(data)
-                                wlog.write(st_time+' '+"mv")
-                                flag=flag+1;
-                            elif flag==3:
-                                data="tftp -p -l BOOT1.bin 192.168.33.60\n"
-                                data=data.encode()
-                                ser.write(data)
-                                wlog.write(st_time+' '+"tftp -p")
+                            #wlog
+                            wlog.write(st_time+' '+response)
+                            #wlog.close()
+                        elif ( 'round-trip' in response ):
+                            print(response)
+                            logtime = time.time()
+                            st_time = datetime.datetime.fromtimestamp(logtime).strftime('%Y-%m-%d %H:%M:%S')
+                            #wlog
+                            wlog.write(st_time+' '+response)
+                            #wlog.close()
+                        #elif ( 'root@se_gsi_apu_18_wnc:~#' in response ):
+                        #    print(response)
+                        #    logtime = time.time()
+                        #    st_time = datetime.datetime.fromtimestamp(logtime).strftime('%Y-%m-%d %H:%M:%S')
+                        #    if flag==0:
+                        #        data='ifconfig eth0 192.168.33.10\n'
+                        #        #data=data.encode("utf-8").decode("cp950","ignore")
+                        #        data=data.encode()
+                        #        ser.write(data)
+                        #        flag=flag+1;
+                        #        wlog.write(st_time+' '+"ifconfig")
+                        #    elif flag==1:
+                        #        data="tftp -g -r BOOT.bin 192.168.33.60\n"
+                        #        data=data.encode()
+                        #        ser.write(data)
+                        #        wlog.write(st_time+' '+"tftp")
+                        #        flag=flag+1;
+                        #    elif flag==2:
+                        #        data="mv BOOT.bin BOOT1.bin\n"
+                        #        data=data.encode()
+                        #        ser.write(data)
+                        #        wlog.write(st_time+' '+"mv")
+                        #        flag=flag+1;
+                        #    elif flag==3:
+                        #        data="tftp -p -l BOOT1.bin 192.168.33.60\n"
+                        #        data=data.encode()
+                        #        ser.write(data)
+                        #        wlog.write(st_time+' '+"tftp -p")
+                        #else:
+                        #    print(response)
                 ser.close()
-                wlog.close()
+                #wlog.close()
 
         except getopt.GetoptError as e1:
                 print ("error communicating...: ",str(e1));
