c52bbf6aafaa64aa54876ad1b32309b2545d6c4c
g893906 <g893906@gmail.com>
2019-12-08 23:24:23 +0800

    time zone1 modify that wait for user to exit

--- time_zone1.py
+++ time_zone1.py
@@ -1,49 +1,52 @@
 #!/usr/bin/python3.6
 
-Time_zone = input("Input the Time Location, TW or IL or ALT:") 
+while 1:
+    Time_zone = input("Input the Time Location, TW or IL or ALT:") 
 
-Time = input("Input the "+Time_zone+" time"", this program will cal. other location time:")
-
-if ('TW' in Time_zone):
-    print( Time_zone + " time is:"+Time )
-    Time = int(Time)
-    if( Time < 13):
-        ALT_time = Time + 11
+    Time = input("Input the "+Time_zone+" time"", this program will cal. other location time:")
+    if ('TW' in Time_zone):
+        print( Time_zone + " time is:"+Time )
+        Time = int(Time)
+        if( Time < 13):
+            ALT_time = Time + 11
+        else:
+            ALT_time = Time - 13
+        if ( Time >= 6 ):
+            Israel_time = Time - 6
+        else:
+            Israel_time = Time - 6 + 24 
+        print("ALT time is:%d" %(ALT_time) )
+        print("Israel time is:%d" %(Israel_time) )
+    
+    elif ('IL' in Time_zone):
+        print( Time_zone + " time is:"+Time )
+        Time = int(Time)
+        if ( Time >= 18 ):
+            TW_time = Time - 18
+        else:
+            TW_time = Time  + 6
+        if( TW_time < 13 ):
+            ALT_time = TW_time + 11
+        else:
+            ALT_time = TW_time - 13
+        print("ALT time is:%d" %(ALT_time) )
+        print("TW time is:%d" %(TW_time) )
+    
+    elif ('ALT' in Time_zone):
+        print( Time_zone + " time is:"+Time )
+        Time = int(Time)
+        if ( Time > 11 ):
+            TW_time = Time - 11
+        else:
+            TW_time = Time  + 13
+        if( TW_time >= 6 ):
+            IL_time = TW_time - 6
+        else:
+            IL_time = TW_time - 6 + 24
+        print("IL time is:%d" %(IL_time) )
+        print("TW time is:%d" %(TW_time) )
     else:
-        ALT_time = Time - 13
-    if ( Time >= 6 ):
-        Israel_time = Time - 6
-    else:
-        Israel_time = Time - 6 + 24 
-    print("ALT time is:%d" %(ALT_time) )
-    print("Israel time is:%d" %(Israel_time) )
-
-elif ('IL' in Time_zone):
-    print( Time_zone + " time is:"+Time )
-    Time = int(Time)
-    if ( Time >= 18 ):
-        TW_time = Time - 18
-    else:
-        TW_time = Time  + 6
-    if( TW_time < 13 ):
-        ALT_time = TW_time + 11
-    else:
-        ALT_time = TW_time - 13
-    print("ALT time is:%d" %(ALT_time) )
-    print("TW time is:%d" %(TW_time) )
-
-elif ('ALT' in Time_zone):
-    print( Time_zone + " time is:"+Time )
-    Time = int(Time)
-    if ( Time > 11 ):
-        TW_time = Time - 11
-    else:
-        TW_time = Time  + 13
-    if( TW_time >= 6 ):
-        IL_time = TW_time - 6
-    else:
-        IL_time = TW_time - 6 + 24
-    print("IL time is:%d" %(IL_time) )
-    print("TW time is:%d" %(TW_time) )
-else:
-    print("Please input the correct format location and time, bye!!!")
+        print("Please input the correct format location and time, bye!!!")
+    print( "\n\n") 
+    print( "!!===========  press CTRL+C to exit ===========!!") 
+    print( "\n\n") 
